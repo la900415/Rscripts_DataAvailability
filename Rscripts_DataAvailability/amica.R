9d810c480b1f43a7d0fcaec56d89cd93a991a9dd
Luis Ariel Espinosa Rodríguez <164960869+la900415@users.noreply.github.com>
2025-07-29 15:18:51 -0300

    Add files via upload

---
+++ Rscripts_DataAvailability/amica.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(shiny)
+
+## Clone the repository using GitGui. Is downloaded to C:/users/Luis Ariel/amica
+#git clone https://github.com/tbaccata/amica.git
+
+## Move to the folder
+#cd amica/amica
+
+## execute install_dependencies.R the first time
+
+
+## Inside R console or R studio
+library("shiny")
+setwd("C:/Users/Luis Ariel/amica/amica")
+runApp()
